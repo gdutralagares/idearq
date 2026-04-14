9e71d101e360b8cffb7705b3789c857c2353409d
gabriel dutra lagares <gabriel.lagares20@gmail.com>
2026-04-14 14:15:37 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Site carregado");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("menu-toggle");
+  const nav = document.getElementById("nav");
+
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    toggle.textContent = nav.classList.contains("active") ? "✕" : "☰";
+  });
+});
