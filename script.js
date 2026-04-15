dfb84686745850dcb679e313d6680e974fed4d66
gabriel dutra lagares <gabriel.lagares20@gmail.com>
2026-04-14 21:45:44 -0300

    update

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const menuToggle = document.getElementById('menu-toggle');
 const nav = document.getElementById('nav');
 const navLinks = document.querySelectorAll('#nav a');
 
-// abrir/fechar menu
+
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('active');
-});
 
-// fechar ao clicar em qualquer link
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    nav.classList.remove('active');
-  });
+  if (menuToggle.classList.contains('active')) {
+    menuToggle.classList.remove('active');
+    menuToggle.textContent = '☰';
+  } else {
+    menuToggle.classList.add('active');
+    menuToggle.textContent = '✕';
+  }
 });
